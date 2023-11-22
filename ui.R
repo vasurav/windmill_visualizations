51722047ffd35939013916933e066d29062beee1
vasurav <ravi.a.vasudevan@gmail.com>
2023-11-22 09:29:42 +0100

    sidebarpanel ui

--- ui.R
+++ ui.R
@@ -1,49 +1,53 @@
 function(request){
-
-fluidPage(theme = shinytheme("darkly"),
-# 
-#     # Application title
-#     titlePanel("Windmill Algorithm vs Real Data"),
     
-    sidebarPanel(
-        selectInput(inputId = "division", "Division:",
-                    choices = c("Mixed", "Open", "Women"), selected = "Mixed"),
-        sliderInput(inputId = "round", "Round:",
-                    min = 1, max = 8, value = 8, step = 1)),
-    
-     mainPanel(
-         tabsetPanel(
-             
-             tabPanel("Team Explorer",
-                      #inputPanel(
-                          uiOutput("team_select"),
-                      #),
-                      textOutput("team_text"),
-                      fluidRow(
-                          column(6,plotlyOutput("plot_real_vs_expected_team")),
-                          column(6,plotlyOutput("plot_ranking_team"))
-                      ),
-                      fluidRow(
-                          column(12, textOutput("team_graph_explainer"), style="padding:25px;")
-                      ),
-                      fluidRow(
-                          column(12,dataTableOutput("team_games_table"))
+    fluidPage(theme = shinytheme("darkly"),
+              # 
+              #     # Application title
+              #     titlePanel("Windmill Algorithm vs Real Data"),
+              
+              sidebarLayout(
+                  conditionalPanel(condition = "input.toggleSidebarPanel % 2 == 0", sidebarPanel(
+                      selectInput(inputId = "division", "Division:",
+                                  choices = c("Mixed", "Open", "Women"), selected = "Mixed"),
+                      sliderInput(inputId = "round", "Round:",
+                                  min = 1, max = 8, value = 8, step = 1))),
+                  
+                  mainPanel(
+                      actionButton("toggleSidebarPanel", "", icon = icon("bars")),
+                      tabsetPanel(
+                          
+                          tabPanel("Team Explorer",
+                                   #inputPanel(
+                                   uiOutput("team_select"),
+                                   #),
+                                   textOutput("team_text"),
+                                   fluidRow(
+                                       column(6,plotlyOutput("plot_real_vs_expected_team")),
+                                       column(6,plotlyOutput("plot_ranking_team"))
+                                   ),
+                                   fluidRow(
+                                       column(12, textOutput("team_graph_explainer"), style="padding:25px;")
+                                   ),
+                                   fluidRow(
+                                       column(12,dataTableOutput("team_games_table"))
+                                   )
+                                   
+                                   
+                          ),
+                          tabPanel("Overview",
+                                   #inputPanel(
+                                   uiOutput("teams_picker"),
+                                   #),
+                                   fluidRow(
+                                       column(6,plotlyOutput("plot_real_vs_expected_overview")),
+                                       column(6,plotlyOutput("plot_ranking_overview"))
+                                   ),
+                                   fluidRow(
+                                       column(12, div("The winning team outperformed the algorithm in blue games and underperformed the algorithm in red games", style="padding:25px;"))
+                                   )
+                          )
                       )
-                      
-                      
-             ),
-             tabPanel("Overview",
-                      #inputPanel(
-                          uiOutput("teams_picker"),
-                      #),
-                      fluidRow(
-                          column(6,plotlyOutput("plot_real_vs_expected_overview")),
-                          column(6,plotlyOutput("plot_ranking_overview"))
-                      ),
-                      fluidRow(
-                          column(12, div("The winning team outperformed the algorithm in blue games and underperformed the algorithm in red games", style="padding:25px;"))
-                      )
-             )
-         )
-     ))
+                  )
+              )
+    )
 }
